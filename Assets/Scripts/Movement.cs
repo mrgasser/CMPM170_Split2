1ef4a4f59d1b15399f9fc26e75a8e3bb0cfabff2
Max <mrgasser@ucsc.edu>
2021-10-29 19:54:49 -0700

    adding project with first build

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Movement : MonoBehaviour
+{
+    
+    [Space]
+    [Header("Booleans")]
+    public bool canMove;
+    public bool wallGrab;
+    public bool wallJumped;
+    public bool wallSlide;
+    public bool isDashing;
+
+}
